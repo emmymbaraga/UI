2c311c642d69a68349694ae3dbc7cde174bf2515
emmymbaraga <emmnish92@gmail.com>
2022-04-09 19:49:34 +0200

    adding script.js,contact us page&footer

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// In contact us section
+document.querySelector('#address').innerHTML = 'Kigali-Rwanda';
+document.querySelector('#street').innerHTML = 'KG';
+document.querySelector('#email').innerHTML = 'info@bng.rw';
+document.querySelector('#phone').innerHTML = '+250785491895';
+
+//Footer
+document.getElementById("copyright").innerHTML="Company name &copy; " + new Date().getFullYear();
+
+// Link to social media
+document.querySelector('#facebook').href = 'http://www.facebook.com';
+document.querySelector('#twitter').href = 'http://www.twitter.com';
+document.querySelector('#instagram').href = 'http://www.instagram.com';
+document.querySelector('#whatsapp').href = 'https://wa.me/+250788384757';
+document.querySelector('#youtube').href = 'http://www.youtube.com';
+document.querySelector('#linkedin').href = 'http://www.linkedin.com';
